00b4dd526ff42961bb7d0fac078adb5627ec9692
Billy Campbell <wcampbell91@gmail.com>
2020-11-19 18:45:33 -0600

    added auth / navbar / and register components

--- src/App/App.js
+++ src/App/App.js
@@ -1,15 +1,25 @@
 import React from 'react';
+import { ApplicationViews } from '../ApplicationViews'
+import {Route, Redirect} from 'react-router-dom'
+import { Login } from '../components/auth/Login'
+import { NavBar } from '../components/nav/NavBar'
+import { Register } from '../components/auth/Register'
 import './App.css';
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className="App">
-        <h2>INSIDE APP COMPONENT</h2>
-        <button className="btn btn-info">I am a button</button>
-      </div>
-    );
-  }
-}
+export const App = () => (
+  <>
+      <Route render={() => {
+          if (localStorage.getItem("lu_token")) {
+              return <>
+                  <Route render={NavBar} />
+                  <Route render={props => <ApplicationViews {...props} />} />
+              </>
+          } else {
+              return <Redirect to="/login" />
+          }
+      }} />
 
-export default App;
+      <Route path="/login" render={Login} />
+      <Route path="/register" render={Register} />
+  </>
+)
---
+++ src/components/nav/NavBar.js
@@ -0,0 +1,36 @@
+
+import React from "react"
+import { Link } from "react-router-dom"
+import "./NavBar.css"
+
+export const NavBar = (props) => {
+    return (
+        <ul className="navbar">
+            <li className="navbar__item">
+                Navigation link
+            </li>
+            <li className="navbar__item">
+                Navigation link
+            </li>
+            <li className="navbar__item">
+                Navigation link
+            </li>
+            {
+                (localStorage.getItem("lu_token") !== null) ?
+                    <li className="nav-item">
+                        <button className="nav-link fakeLink"
+                            onClick={() => {
+                                localStorage.removeItem("lu_token")
+                                props.history.push({ pathname: "/" })
+                            }}
+                        >Logout</button>
+                    </li> :
+                    <>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/login">Login</Link>
+                        </li>
+                    </>
+            }        
+        </ul>
+    )
+}

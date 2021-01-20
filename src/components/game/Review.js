21f0f824f27f0160a2ae8ec8626d16ab3d18253b
Billy Campbell <wcampbell91@gmail.com>
2021-01-19 20:41:17 -0600

    ratings / imageUpload works!

--- src/components/game/Review.js
+++ src/components/game/Review.js
@@ -1,15 +1,22 @@
-import React, { useContext, useEffect } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import { GameContext } from './GameProvider'
 
 export const Review = props => {
-  const { review } = props 
+  const { review, userId } = props 
+  const { getPlayer, player } = useContext(GameContext)
+  const [ currentPlayer, setCurrentPlayer ] = useState({})
+
+  useEffect(() => {
+    getPlayer(userId)
+    setCurrentPlayer(player)
+  }, [])
 
   return (
       <div class="card">
         <div class="card-body">
-          <h5 class="card-title">Review</h5>
-          <p class="card-text">{review.review}</p>
+          {/* <h2>{currentPlayer && currentPlayer.user ? currentPlayer.user.username : ''}</h2> */}
+          <p class="card-text">"{review.review}"</p>
         </div>
       </div>
   )
